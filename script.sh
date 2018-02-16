443b566e79431688aaefd04063f39b535fc585ae
polygamma <jonny.westphalen@googlemail.com>
2018-02-16 23:48:58 +0100

    bye, pacaur :(

--- script.sh
+++ script.sh
@@ -4,22 +4,16 @@
 sudo sh -c "sed -i '/\[multilib\]/,/Include/s/^[ ]*#//' /etc/pacman.conf"
 sudo pacman -Syy
 
-# pacaur
+# aurman
 sudo pacman --needed --noconfirm -Syu git
-gpg --keyserver hkp://ipv4.pool.sks-keyservers.net:11371 --recv-keys 1EB2638FF56C0C53
-git clone https://aur.archlinux.org/cower.git
-cd cower/
+git clone https://aur.archlinux.org/aurman-git.git
+cd aurman-git/
 makepkg -si --needed --noconfirm
 cd ..
-git clone https://aur.archlinux.org/pacaur.git
-cd pacaur/
-makepkg -si --needed --noconfirm
-cd ..
-rm -rf pacaur/
-rm -rf cower/
+rm -rf aurman-git/
 
 # makepkg
-pacaur --needed --noconfirm --noedit -Syu ccache
+aurman --needed --noconfirm --noedit -Syu --pk ccache
 sudo sh -c "sed -i '/^[ ]*BUILDENV=/s/!ccache/ccache/' /etc/makepkg.conf"
 grep "^[ ]*export PATH=\"/usr/lib/ccache/bin/:\$PATH\"" ~/.bashrc >/dev/null
 if [ "$?" -eq 1 ]
@@ -30,12 +24,12 @@ sudo sh -c "sed -i '/MAKEFLAGS=/s/^.*$/MAKEFLAGS=\"-j\$(nproc)\"/' /etc/makepkg.
 sudo sh -c "sed -i '/PKGEXT=/s/^.*$/PKGEXT=\".pkg.tar\"/' /etc/makepkg.conf"
 
 # mirrors
-pacaur --needed --noconfirm --noedit -Syu reflector
+aurman --needed --noconfirm --noedit -Syu --pk reflector
 sudo reflector --save /etc/pacman.d/mirrorlist --sort rate --age 1 --country Germany --latest 10 --score 10 --number 5 --protocol http
-pacaur --noconfirm --noedit -Syyu
+sudo pacman --noconfirm --noedit -Syyu
 
 # xorg + gnome
-pacaur --needed --noconfirm --noedit -Syu xorg-server gnome gdm networkmanager file-roller unrar plank gnome-tweak-tool
+aurman --needed --noconfirm --noedit -Syu --pk xorg-server gnome gdm networkmanager file-roller unrar plank gnome-tweak-tool
 sudo systemctl enable gdm
 sudo systemctl enable NetworkManager
 gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com']"
@@ -66,10 +60,10 @@ chmod 755 ~/.config/autostart
 ln -sf /usr/share/applications/plank.desktop ~/.config/autostart/plank.desktop
 
 # nvidia
-pacaur --needed --noconfirm --noedit -Syu nvidia-dkms lib32-nvidia-utils dkms linux-headers nvidia-settings
+aurman --needed --noconfirm --noedit -Syu --pk nvidia-dkms lib32-nvidia-utils dkms linux-headers nvidia-settings
 
 # intel
-pacaur --needed --noconfirm --noedit -Syu mesa lib32-mesa xf86-video-intel
+aurman --needed --noconfirm --noedit -Syu --pk mesa lib32-mesa xf86-video-intel
 
 # disable ip_v6
 interfaces=$(ip link | sed -n "/^[0-9]\+: \(ens\|eno\|enp\|wl\)[a-zA-Z0-9]\+:.*$/s/^[0-9]\+: \([a-zA-Z0-9]\+\):.*$/\1/p")
@@ -104,7 +98,7 @@ then
 fi
 
 # mouse acceleration disabled with libinput
-pacaur --needed --noconfirm --noedit -Syu libinput xf86-input-libinput xorg-xinput
+aurman --needed --noconfirm --noedit -Syu --pk libinput xf86-input-libinput xorg-xinput
 sudo sh -c "echo 'Section \"InputClass\"
     Identifier \"My Mouse\"
     Driver \"libinput\"
@@ -113,7 +107,7 @@ sudo sh -c "echo 'Section \"InputClass\"
 EndSection' > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf"
 
 # nm-dispatcher-resolv.conf
-pacaur --needed --noconfirm --noedit -Syu git networkmanager-openvpn
+aurman --needed --noconfirm --noedit -Syu --pk git networkmanager-openvpn
 git clone https://github.com/polygamma/nm-dispatcher-resolv.conf.git
 cd nm-dispatcher-resolv.conf/
 makepkg -si --needed --noconfirm
@@ -121,7 +115,7 @@ cd ..
 sudo rm -r nm-dispatcher-resolv.conf/
 
 # ufw
-pacaur --needed --noconfirm --noedit -Syu ufw
+aurman --needed --noconfirm --noedit -Syu --pk ufw
 sudo ufw default deny
 sudo ufw limit ssh
 sudo ufw --force enable
@@ -131,7 +125,7 @@ sudo systemctl enable ufw
 sudo timedatectl set-ntp true
 
 # ssh
-pacaur --needed --noconfirm --noedit -Syu wget openssh
+aurman --needed --noconfirm --noedit -Syu --pk wget openssh
 wget https://pastebin.com/raw/yD2g3ZXv
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -153,29 +147,29 @@ then
 fi
 
 # miscellaneous
-pacaur --needed --noconfirm --noedit -Syu bash-completion asp net-tools ntfs-3g android-tools android-udev dkms linux-headers ttf-google-fonts-git google-chrome woeusb-git jdk8-openjdk keepassx2 rsync dotpac downgrader openconnect networkmanager-openconnect filezilla intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre pycharm-professional clion clion-cmake clion-gdb clion-jre phpstorm phpstorm-jre smartgit
+aurman --needed --noconfirm --noedit -Syu --pk bash-completion asp net-tools ntfs-3g android-tools android-udev dkms linux-headers ttf-google-fonts-git google-chrome woeusb-git jdk8-openjdk keepassx2 rsync dotpac downgrader openconnect networkmanager-openconnect filezilla intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre pycharm-professional clion clion-cmake clion-gdb clion-jre phpstorm phpstorm-jre smartgit
 sudo gpasswd -a $USER adbusers
 
 # sublime
-pacaur --needed --noconfirm --noedit -Syu curl wget
+aurman --needed --noconfirm --noedit -Syu --pk curl wget
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 grep "\[sublime-text\]" /etc/pacman.conf >/dev/null
 if [ "$?" -eq 1 ]
 then
     echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 fi
-pacaur -Syy
-pacaur --needed --noconfirm --noedit -Syu sublime-text
+sudo pacman -Syy
+aurman --needed --noconfirm --noedit -Syu --pk sublime-text
 wget https://pastebin.com/raw/n64DScny
 install -Dm700 n64DScny "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
 rm n64DScny
 
 # themes
-pacaur --needed --noconfirm --noedit -Syu paper-icon-theme-git arc-gtk-theme
+aurman --needed --noconfirm --noedit -Syu --pk paper-icon-theme-git arc-gtk-theme
 gsettings set org.gnome.shell.extensions.user-theme name "Arc"
 gsettings set org.gnome.desktop.interface gtk-theme "Arc"
 gsettings set org.gnome.desktop.interface icon-theme "Paper"
 gsettings set org.gnome.desktop.interface cursor-theme "Paper"
 
 # signal
-pacaur --needed --noconfirm --noedit -Syu signal-desktop-bin
+aurman --needed --noconfirm --noedit -Syu --pk signal-desktop-bin
