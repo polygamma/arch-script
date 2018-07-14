074475fd7658ff8c1b054371ac77065e7214a10c
Jonni Westphalen <jonny.westphalen@googlemail.com>
2018-07-14 18:40:30 +0200

    add --skip_news to install script for aurman

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ mkdir -p ~/.config/aurman/
 printf "[miscellaneous]\nshow_changes\ndo_everything\nuse_ask\nkeyserver=hkp://ipv4.pool.sks-keyservers.net:11371\n" > ~/.config/aurman/aurman_config
 
 # makepkg
-aurman --needed --noconfirm --noedit -Syu ccache
+aurman --needed --noconfirm --noedit --skip_news -Syu ccache
 sudo sh -c "sed -i '/^[ ]*BUILDENV=/s/!ccache/ccache/' /etc/makepkg.conf"
 grep "^[ ]*export PATH=\"/usr/lib/ccache/bin/:\$PATH\"" ~/.bashrc >/dev/null
 if [ "$?" -eq 1 ]
@@ -32,12 +32,12 @@ sudo sh -c "sed -i '/MAKEFLAGS=/s/^.*$/MAKEFLAGS=\"-j\$(nproc)\"/' /etc/makepkg.
 sudo sh -c "sed -i '/PKGEXT=/s/^.*$/PKGEXT=\".pkg.tar\"/' /etc/makepkg.conf"
 
 # mirrors
-aurman --needed --noconfirm --noedit -Syu reflector
+aurman --needed --noconfirm --noedit --skip_news -Syu reflector
 sudo reflector --save /etc/pacman.d/mirrorlist --sort rate --age 1 --country Germany --latest 10 --score 10 --number 5 --protocol http
 sudo pacman --noconfirm -Syyu
 
 # xorg + gnome
-aurman --needed --noconfirm --noedit -Syu xorg-server gnome gdm networkmanager file-roller unrar plank gnome-tweaks
+aurman --needed --noconfirm --noedit --skip_news -Syu xorg-server gnome gdm networkmanager file-roller unrar plank gnome-tweaks
 sudo systemctl enable gdm
 sudo systemctl enable NetworkManager
 gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com']"
@@ -68,10 +68,10 @@ chmod 755 ~/.config/autostart
 ln -sf /usr/share/applications/plank.desktop ~/.config/autostart/plank.desktop
 
 # nvidia
-aurman --needed --noconfirm --noedit -Syu nvidia-dkms lib32-nvidia-utils dkms linux-headers nvidia-settings
+aurman --needed --noconfirm --noedit --skip_news -Syu nvidia-dkms lib32-nvidia-utils dkms linux-headers nvidia-settings
 
 # intel
-aurman --needed --noconfirm --noedit -Syu mesa lib32-mesa xf86-video-intel
+aurman --needed --noconfirm --noedit --skip_news -Syu mesa lib32-mesa xf86-video-intel
 
 # disable ip_v6
 interfaces=$(ip link | sed -n "/^[0-9]\+: \(ens\|eno\|enp\|wl\)[a-zA-Z0-9]\+:.*$/s/^[0-9]\+: \([a-zA-Z0-9]\+\):.*$/\1/p")
@@ -106,7 +106,7 @@ then
 fi
 
 # mouse acceleration disabled with libinput
-aurman --needed --noconfirm --noedit -Syu libinput xf86-input-libinput xorg-xinput
+aurman --needed --noconfirm --noedit --skip_news -Syu libinput xf86-input-libinput xorg-xinput
 sudo sh -c "echo 'Section \"InputClass\"
     Identifier \"My Mouse\"
     Driver \"libinput\"
@@ -115,7 +115,7 @@ sudo sh -c "echo 'Section \"InputClass\"
 EndSection' > /etc/X11/xorg.conf.d/50-mouse-acceleration.conf"
 
 # nm-dispatcher-resolv.conf
-aurman --needed --noconfirm --noedit -Syu git networkmanager-openvpn
+aurman --needed --noconfirm --noedit --skip_news -Syu git networkmanager-openvpn
 git clone https://github.com/polygamma/nm-dispatcher-resolv.conf.git
 cd nm-dispatcher-resolv.conf/
 makepkg -si --needed --noconfirm
@@ -123,7 +123,7 @@ cd ..
 rm -rf nm-dispatcher-resolv.conf/
 
 # ufw
-aurman --needed --noconfirm --noedit -Syu ufw
+aurman --needed --noconfirm --noedit --skip_news -Syu ufw
 sudo ufw default deny
 sudo ufw limit ssh
 sudo ufw --force enable
@@ -133,7 +133,7 @@ sudo systemctl enable ufw
 sudo timedatectl set-ntp true
 
 # ssh
-aurman --needed --noconfirm --noedit -Syu wget openssh
+aurman --needed --noconfirm --noedit --skip_news -Syu wget openssh
 wget https://pastebin.com/raw/yD2g3ZXv
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -155,11 +155,11 @@ then
 fi
 
 # miscellaneous
-aurman --needed --noconfirm --noedit -Syu bash-completion asp lxsplit net-tools ntfs-3g android-tools android-udev dkms linux-headers ttf-google-fonts-git google-chrome woeusb-git jdk8-openjdk keepassx2 rsync dotpac downgrader openconnect networkmanager-openconnect filezilla intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre pycharm-professional clion clion-cmake clion-gdb clion-jre phpstorm phpstorm-jre android-studio smartgit
+aurman --needed --noconfirm --noedit --skip_news -Syu bash-completion asp lxsplit net-tools ntfs-3g android-tools android-udev dkms linux-headers ttf-google-fonts-git google-chrome woeusb-git jdk8-openjdk keepassx2 rsync dotpac downgrader openconnect networkmanager-openconnect filezilla intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre pycharm-professional clion clion-cmake clion-gdb clion-jre phpstorm phpstorm-jre android-studio smartgit
 sudo gpasswd -a $USER adbusers
 
 # sublime
-aurman --needed --noconfirm --noedit -Syu curl wget
+aurman --needed --noconfirm --noedit --skip_news -Syu curl wget
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 grep "\[sublime-text\]" /etc/pacman.conf >/dev/null
 if [ "$?" -eq 1 ]
@@ -167,7 +167,7 @@ then
     echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 fi
 sudo pacman -Syy
-aurman --needed --noconfirm --noedit -Syu sublime-text
+aurman --needed --noconfirm --noedit --skip_news -Syu sublime-text
 wget https://pastebin.com/raw/n64DScny
 install -Dm700 n64DScny "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
 rm n64DScny
@@ -178,11 +178,11 @@ then
 fi
 
 # themes
-aurman --needed --noconfirm --noedit -Syu paper-icon-theme-git arc-gtk-theme
+aurman --needed --noconfirm --noedit --skip_news -Syu paper-icon-theme-git arc-gtk-theme
 gsettings set org.gnome.shell.extensions.user-theme name "Arc"
 gsettings set org.gnome.desktop.interface gtk-theme "Arc"
 gsettings set org.gnome.desktop.interface icon-theme "Paper"
 gsettings set org.gnome.desktop.interface cursor-theme "Paper"
 
 # signal
-aurman --needed --noconfirm --noedit -Syu signal-desktop-bin
+aurman --needed --noconfirm --noedit --skip_news -Syu signal-desktop-bin
