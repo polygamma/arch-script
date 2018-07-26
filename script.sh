4a27bf808a7f788fab654233c6d3a7c9aedda6af
Jonni Westphalen <jonny.westphalen@googlemail.com>
2018-07-26 15:16:26 +0200

    change aurman config

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ rm -rf aurman-git/
 
 # aurman config
 mkdir -p ~/.config/aurman/
-printf "[miscellaneous]\ndo_everything\nuse_ask\nkeyserver=hkp://ipv4.pool.sks-keyservers.net:11371\n" > ~/.config/aurman/aurman_config
+printf "[miscellaneous]\nshow_changes\ndo_everything\nuse_ask\nkeyserver=hkp://ipv4.pool.sks-keyservers.net:11371\n" > ~/.config/aurman/aurman_config
 
 # makepkg
 aurman --needed --noconfirm --noedit --skip_news --skip_new_locations --pgp_fetch -Syu ccache
@@ -186,6 +186,3 @@ gsettings set org.gnome.desktop.interface cursor-theme "Paper"
 
 # signal
 aurman --needed --noconfirm --noedit --skip_news --skip_new_locations --pgp_fetch -Syu signal-desktop-bin
-
-# aurman config final
-printf "[miscellaneous]\nshow_changes\ndo_everything\nuse_ask\nkeyserver=hkp://ipv4.pool.sks-keyservers.net:11371\n" > ~/.config/aurman/aurman_config
